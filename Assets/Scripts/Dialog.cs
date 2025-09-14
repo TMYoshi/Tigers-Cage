3a978605c61d3b73c27e7413539d15ed6d2f06b8
greene11-poe <ndang7863@gmail.com>
2025-09-14 15:20:24 -0700

    Room mockup & Dialog speed fix
    quick room idea + some security checks to ensure dialog finishes before item is hidden/deleted from view

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -11,8 +11,10 @@ public class Dialog : MonoBehaviour
     public float text_speed_ = 0.1f;
     public string text_name_ = "Test";
     public Animator dialog_animator_;
+
     private bool dialog_opener_ = true;
     private int index_ = 0;
+    private bool is_dialog_ending_ = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +25,11 @@ public class Dialog : MonoBehaviour
     // Update is called once per frame
     public bool PlayDialog()
     {
+        // prevent input during dialog ending
+        if (is_dialog_ending_)
+        {
+            return false;
+        }
         if (dialog_opener_)
         {
             dialog_opener_ = false;
@@ -42,7 +49,7 @@ public class Dialog : MonoBehaviour
             text_component_.text = text_name_ + '\n' + lines_[index_];
         }
 
-        return true;
+        return false; // changed from true, not ending dialog here anymore just finishing current line
     }
 
     public void StartDialogue()
@@ -50,12 +57,15 @@ public class Dialog : MonoBehaviour
         gameObject.SetActive(true);
         dialog_animator_.SetTrigger("Enter");
         index_ = 0;
+        is_dialog_ending_ = false; // flag reset
         ResetText();
         StartCoroutine(TypeLine());
     }
 
     public bool NextLine()
     {
+        if (is_dialog_ending_) return true;
+       
         dialog_animator_.SetTrigger("Bob Sprite");
         if (index_ < lines_.Count - 1)
         {
@@ -67,11 +77,13 @@ public class Dialog : MonoBehaviour
         }
         else
         {
+            is_dialog_ending_ = true;
             Debug.Log("Finishing Dialog");
             text_component_.text = "";
             dialog_animator_.SetTrigger("Exit"); // TODO: Fix so actually triggers animation lol
             dialog_opener_ = true;
-            gameObject.SetActive(false);
+            is_dialog_ending_ = false; // reset again
+            gameObject.SetActive(false); // hide dialog until next item is clicked
             return true;
         }
     }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,10 +6,30 @@ public class Item : MonoBehaviour
     public List<string> lines_;
     public Dialog assoc_dialog_box_;
 
+    private float LastInteractionTime = 0f;
+    private float InteractionCooldown = 0.2f; // .2s
+
     public bool WriteLines()
     {
+        if (Time.time - LastInteractionTime < InteractionCooldown)
+        {
+            return false;
+        }
+
         Debug.Log("Click!");
+        LastInteractionTime = Time.time;
+
         assoc_dialog_box_.SetLines(lines_);
-        return assoc_dialog_box_.PlayDialog();
+        bool dialog_finished = assoc_dialog_box_.PlayDialog();
+
+        if (dialog_finished)
+        {
+            InventoryItem _inventoryItem = this as InventoryItem;
+            if(_inventoryItem != null && _inventoryItem.Collectable)
+            {
+                Destroy(gameObject); // deleting item
+            }
+        }
+        return dialog_finished;
     }
 }
