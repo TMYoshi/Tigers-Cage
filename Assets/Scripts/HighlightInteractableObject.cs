42b51ccbd14f50a5d6d8ffa18c424c9b6ec286f1
Fish <thehuiq2006@gmail.com>
2025-10-19 22:32:13 -0700

    Feat: Highlight shader

--- Assets/Scripts/HighlightInteractableObject.cs
+++ Assets/Scripts/HighlightInteractableObject.cs
@@ -16,90 +16,27 @@ using UnityEngine;
 
 public class HighlightInteractableOutline : MonoBehaviour
 {
-    [Header("Outline Settings")]
-    public Color outlineColor = Color.black; // change later based on sprite
-    public float outlineWidth = 0.1f;
-    [Range(4, 16)]
-    /* some sprites play well with this method, some don't; may need to revisit 
-    unsure what balance to strike between resolution, outlineThickness for performance
-    since it creates all the copies on startup */
-
-    public int outlineResolution = 8; // how many outline copies to create
-
+    [SerializeField] Material highlight, nonHighlight;
     private SpriteRenderer spriteRenderer;
-    private GameObject outlineParent;
-
-    void Start() 
+    [SerializeField] private Color outlineColor = Color.black;
+    [SerializeField] private float outlineSize = 0.01f;
+    void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        CreateOutlineEffect();
     }
-
-    void CreateOutlineEffect()
-    {
-        // create a parent object to hold created outline sprites
-        outlineParent = new GameObject(gameObject.name + "_OutlineGroup");
-        outlineParent.transform.SetParent(transform); // makes outline parent a child of this GameObject
-        outlineParent.transform.localPosition = Vector3.zero; // sets pos to (0,0,0) relative to parent
-        outlineParent.transform.localScale = Vector3.one; // sets scale to (1,1,1)
-        outlineParent.SetActive(false); // start hidden
-
-        for (int i = 0; i < outlineResolution; i++)
-        {
-            float angle = (360f / outlineResolution) * i;
-            Vector3 direction = new Vector3(
-                Mathf.Cos(angle * Mathf.Deg2Rad), // x = cos
-                Mathf.Sin(angle * Mathf.Deg2Rad), // y = sin
-                0 // no z in 2d :)
-            ) * outlineWidth;
-
-            direction = transform.rotation * direction;
-
-            // create outline sprite GameObject
-            GameObject outlineSprite = new GameObject($"Outline_{i}");
-            outlineSprite.transform.SetParent(outlineParent.transform);
-            outlineSprite.transform.localPosition = direction;
-            outlineSprite.transform.localScale = Vector3.one;
-
-            outlineSprite.transform.rotation = transform.rotation;
-
-            // add and config SpriteRenderer
-            SpriteRenderer outlineSR = outlineSprite.AddComponent<SpriteRenderer>();
-            outlineSR.sprite = spriteRenderer.sprite;
-            outlineSR.color = outlineColor;
-
-            // sort and re-layer 
-            outlineSR.sortingLayerName = spriteRenderer.sortingLayerName;
-            outlineSR.sortingOrder = spriteRenderer.sortingOrder - 1;
-        }
-
-        Debug.Log($"Created outline effect with {outlineResolution} sprites.");
-    }
-
     public void Enter()
     {
-        // show outline
-        if (outlineParent != null)
-        {
-            outlineParent.SetActive(true);
-        }
-        Debug.Log("Mouse entered " + gameObject.name);
+        highlight.SetColor("_OutlineColor", outlineColor);
+        highlight.SetFloat("_Size", outlineSize);
+        spriteRenderer.material = highlight;
     }
 
     public void Exit()
     {
-        // hide outline
-        if (outlineParent != null)
-        {
-            outlineParent.SetActive(false);
-        }
+        spriteRenderer.material = nonHighlight;
     }
     void OnDestroy()
     {
-        // clean up outlines
-        if (outlineParent != null)
-        {
-            DestroyImmediate(outlineParent);
-        }
+        spriteRenderer.material = nonHighlight;
     }
 }
