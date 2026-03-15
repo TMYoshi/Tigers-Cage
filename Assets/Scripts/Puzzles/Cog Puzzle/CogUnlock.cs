2185e9a9f87c0bf4130ccd8c94471432aa4a3a77
Nick Dang <ndang7863@gmail.com>
2026-03-15 12:41:08 -0700

    Feature: Cog ownership checking for redux

--- Assets/Scripts/Puzzles/Cog Puzzle/CogUnlock.cs
+++ Assets/Scripts/Puzzles/Cog Puzzle/CogUnlock.cs
@@ -2,8 +2,37 @@ using UnityEngine;
 
 public class CogUnlock : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private GameObject mediumLockedCog;
+    [SerializeField] private GameObject largeLockedCog;
+
+    private void Start()
     {
-        
+        CheckAndUnlock("Medium Cog", mediumLockedCog);
+        CheckAndUnlock("Large Cog", largeLockedCog);
+    }
+
+    private void CheckAndUnlock(string itemName, GameObject cogObject)
+    {
+        bool hasItem = false;
+
+        foreach (string id in InventoryManager.collectedItems)
+        {
+            if (id.Contains(itemName))
+            {
+                hasItem = true;
+                break;
+            }
+        }
+
+        if (hasItem)
+        {
+            cogObject.SetActive(true);
+            Debug.Log($"Success, {itemName} found in inventory.");
+        }
+        else
+        {
+            cogObject.SetActive(false);
+            Debug.Log($"Fail, {itemName} found in inventory.");
+        }
     }
 }
