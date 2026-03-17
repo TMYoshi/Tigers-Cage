52f46190715e286a296900644c7ba1bf9a7aa018
TMYoshi <143219982+HiepSolivan@users.noreply.github.com>
2026-03-16 18:01:23 -0700

    SFX Managers added

--- Assets/Scripts/Sound_Mixer_Manager.cs
+++ Assets/Scripts/Sound_Mixer_Manager.cs
@@ -1,24 +1,52 @@
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 public class Sound_Mixer_Manager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audio_mixer_;
+    [Tooltip("All these attributes are manually derived from the Pause UI Settings tab")]
+    [SerializeField] private Slider master_slider_;
+    [SerializeField] private Slider sfx_slider_;
+    [SerializeField] private Slider music_slider_;
 
-    public void SetMasterVolume(float level)
+    private void Start()
     {
+        if (PlayerPrefs.HasKey("Master Volume") || PlayerPrefs.HasKey("SFX Volume") || PlayerPrefs.HasKey("Music Volume"))
+        {
+            Debug.Log("Setting volume");
+            LoadVolume();
+        }
+    }
+
+    public void SetMasterVolume()
+    {
+        float level = master_slider_.value;
+        PlayerPrefs.SetFloat("Master Volume", level);
         audio_mixer_.SetFloat("MasterVolume", Mathf.Log10(level) * 20f);
     }
 
-    public void SetSFXVolume(float level)
+    public void SetSFXVolume()
     {
+        float level = sfx_slider_.value;
         audio_mixer_.SetFloat("SFXVolume", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("SFX Volume", level);
 
     }
     
-    public void SetMusicVolume(float level)
+    public void SetMusicVolume()
     {
+        float level = music_slider_.value;
         audio_mixer_.SetFloat("MusicVolume", Mathf.Log10(level) * 20f);
-        
+        PlayerPrefs.SetFloat("Music Volume", level);
+    }
+    private void LoadVolume()
+    {
+        master_slider_.value = PlayerPrefs.GetFloat("Master Volume");
+        sfx_slider_.value = PlayerPrefs.GetFloat("SFX Volume");
+        music_slider_.value = PlayerPrefs.GetFloat("Music Volume");
+
+        SetMasterVolume();
+        SetSFXVolume();
+        SetMusicVolume();
     }
 }
