7eafa76e4c8065bd747dfd090e7e1ab3c1292700
Fish <126228760+NomMilk@users.noreply.github.com>
2026-04-20 03:40:59 -0700

    feat: slice sound

--- Assets/Scripts/Item/SpecialItems/UniqItem/_Transition_Item.cs
+++ Assets/Scripts/Item/SpecialItems/UniqItem/_Transition_Item.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class _Transition_Item : SpecialItems
 {
     public string transition_to_;
+    [SerializeField] AudioClip audio_clip_;
+    bool already_played_ = false;
+
     public override void EnterCondition()
     {
+        if(already_played_) return;
+
+        if(audio_clip_ != null) SFXManager.Instance.PlaySFXClip(audio_clip_);
+        already_played_ = true;
+
         Debug.Log(transition_to_);
         if (transition_to_ != "")
         {
