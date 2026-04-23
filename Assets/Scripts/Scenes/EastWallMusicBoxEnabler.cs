28acab693043f688eea5e3791d5eaf7c0e9a22a4
Nick Dang <ndang7863@gmail.com>
2026-04-23 16:19:39 -0700

    Feature: Batteries+Camera crafting, South merge fixes

--- Assets/Scripts/Scenes/EastWallMusicBoxEnabler.cs
+++ Assets/Scripts/Scenes/EastWallMusicBoxEnabler.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class EastWallMusicBoxEnabler : MonoBehaviour
 {
-    // more new scripts lmao sorry 
     void Start()
     {
         if (CutsceneManager.musicBoxCutsceneCompleted)
         {
             GameObject items = GameObject.Find("Items");
+            GameObject collectibles = GameObject.Find("Collectable Items");
 
             if (items != null)
             {
                 Transform brokenBox = items.transform.Find("Broken_Music_Box");
                 if (brokenBox != null) brokenBox.gameObject.SetActive(true);
+            }
+
+            if (collectibles != null)
+            {
+                Transform batteries = collectibles.transform.Find("Batteries");
+                if (batteries != null) batteries.gameObject.SetActive(true);
             }
         }
     }
