2ce17a7951c351c9d15a3c96ddbd56e11c863e2e
TMYoshi <yoshitheanimator@gmail.com>
2026-04-23 21:39:53 -0700

    Brightness Consistent Thru Rooms

--- Assets/Scripts/Brightness_Slider.cs
+++ Assets/Scripts/Brightness_Slider.cs
@@ -1,50 +1,21 @@
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
-public class Brightness_Slider : MonoBehaviour
+// Slider class that communicates with manager
+public class BrightnessSlider : MonoBehaviour
 {
     public Slider slider_;
-    public Volume volume_;
-    private VolumeProfile volume_profile_;
-    private ColorAdjustments brightness_;
+    private BrightnessManager brightness_manager_;
 
     private void Awake()
     {
-        volume_ = FindAnyObjectByType<Volume>();
+        brightness_manager_ = FindAnyObjectByType<BrightnessManager>();
+        if(brightness_manager_ == null) { Debug.LogError("Brightness Manager not available"); }
     }
-
-    private void Start()
+    
+    public void SliderBrightness()
     {
-        volume_profile_ = volume_.profile;
-        volume_profile_.TryGet<ColorAdjustments>(out brightness_);
-
-        if(PlayerPrefs.HasKey("Brightness"))
-        {
-            SetBrightness(PlayerPrefs.GetFloat("Brightness"));
-        }
-    }
-
-    public void SetBrightness(float value)
-    {
-        Gradient gradient = new Gradient();
-
-        var colors = new GradientColorKey[3];
-        colors[0] = new GradientColorKey(Color.white, 1.0f);
-        colors[1] = new GradientColorKey(Color.gray4, 0.5f);
-        colors[2] = new GradientColorKey(Color.gray1, 0.0f);
-
-        gradient.SetColorKeys(colors);
-
-        if(value >= 0f)
-        {
-            brightness_.colorFilter.value = gradient.Evaluate(value);
-            PlayerPrefs.SetFloat("Brightness", value);
-        }
-        else
-        {
-            brightness_.colorFilter.value = gradient.Evaluate(0.5f);
-        }
+        float value = slider_.value;
+        brightness_manager_.SetBrightnessValue(value);
     }
 }
