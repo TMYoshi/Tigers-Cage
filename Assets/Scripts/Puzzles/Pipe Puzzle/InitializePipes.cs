dd0166d8c1bbac40e146fef6234cf76c79de1a82
Nick Dang <ndang7863@gmail.com>
2026-07-30 19:11:49 -0700

    Feature: PipePuzzle randomization and updated assets

--- Assets/Scripts/Puzzles/Pipe Puzzle/InitializePipes.cs
+++ Assets/Scripts/Puzzles/Pipe Puzzle/InitializePipes.cs
@@ -2,35 +2,79 @@ using UnityEngine;
 
 public class InitializePipes : MonoBehaviour
 {
-    int[] rotationAmounts = { 0, 90, 180, 270};
+    [SerializeField] private Transform pipesParent;
 
-     /*
-     * solution:
-     * 1, 90
-     * 2, 180
-     * 3, 270
-     * 4, 180
-     * 5, 90
-     * 6, 90
-     * 7, 90
-     * 8, 0
-     * 9, 0
-     * 10, 180
-     * 11, 90
-     * 12, 180
-     * 13, 270
-     * 14, 180
-     * 15, 90
-     * 16, 270
-     * 
-     * 0: 8,9
-     * 90: 1,5,6,7,11,15
-     * 180: 2,4,10,12,14
-     * 270: 3,13,16
-     */
+    int[] rotationAmounts = { 0, 90, 180, 270 };
 
-    void Start()
+    /*
+    Solution:
+    90
+    0
+    180
+    0
+    0
+
+    90
+    180
+    0
+    180
+    270
+
+    90
+    90
+    180
+    0
+    90
+
+    270
+    270
+    90
+    270
+    270
+
+    270
+    180
+    180
+    180
+    270
+
+    90:  1,6,11,12,15,18
+    180: 3,7,9,13,22,23,24
+    270: 10,16,17,19,20,21,25
+    0:   2,4,5,8,14
+    */
+
+    private void Start()
     {
-        
+        RandomizePipes();
+    }
+
+    public void RandomizePipes()
+    {
+        if (pipesParent == null)
+        {
+            Transform foundParent = transform.parent?.Find("Pipes");
+            if (foundParent != null) pipesParent = foundParent;
+            else return;
+        }
+
+        float[] pipeZRotations = new float[pipesParent.childCount];
+
+        for (int i = 0;  i < pipesParent.childCount; i++)
+        {
+            int randomIndex = Random.Range(0, 4);
+            int randomZRotation = rotationAmounts[randomIndex];
+            Transform pipe = pipesParent.GetChild(i);
+
+            if (pipe.TryGetComponent<PipeRotation>(out PipeRotation pipeScript))
+            {
+                pipeScript.SetRotation(randomZRotation);
+            }
+
+            Vector3 currentEuler = pipe.eulerAngles;
+            pipe.eulerAngles = new Vector3(currentEuler.x, currentEuler.y, randomZRotation);
+
+            pipeZRotations[i] = pipe.eulerAngles.z;
+        }
     }
 }
